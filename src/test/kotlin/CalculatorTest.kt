f4de26dbb4d2d746daec9435218e419b17e90a13
NachoBonilla03 <ignacio.bonilla.rojas@est.una.ac.cr>
2025-02-23 12:25:04 -0600

    Substraction tests ready

--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -41,7 +41,7 @@ class CalculatorTest {
 
 
     @Test //Addition with zero
-    @DisplayName("Should return the sum of 2 negative numbers")
+    @DisplayName("Should return the sum of a negative number and zero")
     fun testZeroAddition() {
         // Arrange
         val a:Double = -3.0
@@ -49,30 +49,77 @@ class CalculatorTest {
         // Act
         val result:Double  = calculator.add(a, b)
         // Assert
-        assertEquals(-3.0, result, "-3.0 + 0.0 should equal -5.0")
+        assertEquals(-3.0, result, "-3.0 + 0.0 should equal -3.0")
     }
 
     @Test //Addition with edge values
-    @DisplayName("Should return the sum of 2 negative numbers")
+    @DisplayName("Should return the sum of 2 edge values")
     fun testEdgeValuesAddition() {
         // Arrange
         val a:Double =  999999999999.0
         val b:Double  = 0.000000002
         // Act
         val result:Double  = calculator.add(a, b)
+        val expected:Double = 999999999999.000000002
+        val tolerance:Double =  0.000000001
         // Assert
-        assertEquals(999999999999.000000002, result, "1_000_000_000.0 + 0.000000002 should equal 999999999999.000000002")
+        assertEquals(expected, result,tolerance, "999999999999.0 +0.000000002 should equal 999999999999.000000002")
     }
 
 
 
-
-
-    // TODO: Create your subtraction tests
-    @Test
-    fun testSubtraction() {
-        // Implement your test
+    //Test cases for substraction  operations
+    @Test  //Positive values test
+    @DisplayName("Should return the sub of 2 positive numbers")
+    fun testPositiveSubtraction() {
+        // Arrange
+        val a:Double  = 3.0
+        val b:Double = 2.0
+        // Act
+        val result:Double  = calculator.subtract(a, b)
+        // Assert
+        assertEquals(1.0, result, "3.0 - 2.0 should equal 1.0")
     }
+
+    @Test //Negative values test
+    @DisplayName("Should return the sub of 2 negative numbers")
+    fun testNegativeSubtraction() {
+        // Arrange
+        val a:Double  = -3.0
+        val b:Double  = -2.0
+        // Act
+        val result:Double  = calculator.subtract(a, b)
+        // Assert
+        assertEquals(-1.0, result, "-3.0 - -2.0 should equal -1.0")
+    }
+
+
+    @Test //Subtraction with zero
+    @DisplayName("Should return the sub of a negative number and zero")
+    fun testZeroSubtraction() {
+        // Arrange
+        val a:Double = -3.0
+        val b:Double  = 0.0
+        // Act
+        val result:Double  = calculator.subtract(a, b)
+        // Assert
+        assertEquals(-3.0, result, "-3.0 - 0.0 should equal -3.0")
+    }
+
+    @Test //Subtraction with edge values
+    @DisplayName("Should return the sub of 2 edge values")
+    fun testEdgeValuesSubtraction() {
+        // Arrange
+        val a:Double =  999999999.0
+        val b:Double  = 0.0002
+        // Act
+        val result:Double  = calculator.subtract(a, b)
+        val expected:Double = 999999998.9998
+        val tolerance:Double =  0.00001
+        // Assert
+        assertEquals(expected, result,tolerance, "999999999.0 - 0.0002 should equal 999999998.9998")
+    }
+
 
     // TODO: Create your multiplication tests
     @Test
